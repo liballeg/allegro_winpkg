dfaef7f606b646668f661bf769f114333f26f7a0
Pavel Sountsov <siege@google.com>
2015-04-26 14:55:55 -0700

    Fix the slashes.

--- universal/build_allegro_msvc_32.bat
+++ universal/build_allegro_msvc_32.bat
@@ -1,7 +1,7 @@
-set deps=%cd%/deps_output_msvc_32
-set output=%cd%/allegro_output_msvc_32
+set deps=%cd%\deps_output_msvc_32
+set output=%cd%\allegro_output_msvc_32
 set generator="Visual Studio 12 2013"
 set toolchain=v120_xp
-set build_dir=%cd%/build_msvc_32
+set build_dir=%cd%\build_msvc_32
 
 build_allegro_msvc.bat
--- universal/build_allegro_msvc_64.bat
+++ universal/build_allegro_msvc_64.bat
@@ -1,7 +1,7 @@
-set deps=%cd%/deps_output_msvc_64
-set output=%cd%/allegro_output_msvc_64
+set deps=%cd%\deps_output_msvc_64
+set output=%cd%\allegro_output_msvc_64
 set generator="Visual Studio 12 2013 Win64"
 set toolchain=v120_xp
-set build_dir=%cd%/build_msvc_64
+set build_dir=%cd%\build_msvc_64
 
 build_allegro_msvc.bat
