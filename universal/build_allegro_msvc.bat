cc9eaf3deda1a5ba3518dc5a5ffde7410f8f2181
Peter Hull <peterhull90@gmail.com>
2015-09-22 13:39:03 +0100

    Quote all directories to allow for them containing spaces. Also, the shell can make subdirectories without needing to make the intermediate directories.

--- universal/build_allegro_msvc.bat
+++ universal/build_allegro_msvc.bat
@@ -1,23 +1,21 @@
 set root=%cd%
 set common_args=-G %generator% -T %toolchain% -DCMAKE_PREFIX_PATH=%deps% -DCMAKE_INSTALL_PREFIX=%output% -DWANT_EXAMPLES=off -DWANT_TESTS=off -DWANT_DEMO=off
 
-mkdir %build_dir%
+mkdir "%build_dir%\allegro"
+cd "%build_dir%\allegro"
 
-mkdir %build_dir%\allegro
-cd %build_dir%\allegro
-
-cmake %root%\allegro -DWANT_MONOLITH=off -DSHARED=on -DWANT_STATIC_RUNTIME=off -DCMAKE_BUILD_TYPE=RelWithDebInfo %common_args% || goto :error
+cmake "%root%\allegro" -DWANT_MONOLITH=off -DSHARED=on -DWANT_STATIC_RUNTIME=off -DCMAKE_BUILD_TYPE=RelWithDebInfo %common_args% || goto :error
 cmake --build . --target INSTALL --config RelWithDebInfo  || goto :error
-cmake %root%\allegro -DWANT_MONOLITH=on -DSHARED=on -DWANT_STATIC_RUNTIME=off -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake "%root%\allegro" -DWANT_MONOLITH=on -DSHARED=on -DWANT_STATIC_RUNTIME=off -DCMAKE_BUILD_TYPE=RelWithDebInfo
 cmake --build . --target INSTALL --config RelWithDebInfo  || goto :error
-cmake %root%\allegro -DWANT_MONOLITH=off -DSHARED=off -DWANT_STATIC_RUNTIME=on -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake "%root%\allegro" -DWANT_MONOLITH=off -DSHARED=off -DWANT_STATIC_RUNTIME=on -DCMAKE_BUILD_TYPE=RelWithDebInfo
 cmake --build . --target INSTALL --config RelWithDebInfo  || goto :error
-cmake %root%\allegro -DWANT_MONOLITH=on -DSHARED=off -DWANT_STATIC_RUNTIME=on -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake "%root%\allegro" -DWANT_MONOLITH=on -DSHARED=off -DWANT_STATIC_RUNTIME=on -DCMAKE_BUILD_TYPE=RelWithDebInfo
 cmake --build . --target INSTALL --config RelWithDebInfo  || goto :error
 
-cmake %root%\allegro -DWANT_MONOLITH=off -DSHARED=on -DWANT_STATIC_RUNTIME=off -DCMAKE_BUILD_TYPE=Debug
+cmake "%root%\allegro" -DWANT_MONOLITH=off -DSHARED=on -DWANT_STATIC_RUNTIME=off -DCMAKE_BUILD_TYPE=Debug
 cmake --build . --target INSTALL --config Debug  || goto :error
-cmake %root%\allegro -DWANT_MONOLITH=on -DSHARED=on -DWANT_STATIC_RUNTIME=off -DCMAKE_BUILD_TYPE=Debug
+cmake "%root%\allegro" -DWANT_MONOLITH=on -DSHARED=on -DWANT_STATIC_RUNTIME=off -DCMAKE_BUILD_TYPE=Debug
 cmake --build . --target INSTALL --config Debug  || goto :error
 ::~ cmake %root%\allegro -DWANT_MONOLITH=off -DSHARED=off -DWANT_STATIC_RUNTIME=on -DCMAKE_BUILD_TYPE=Debug
 ::~ cmake --build . --target INSTALL --config Debug  || goto :error
--- universal/build_deps_msvc.bat
+++ universal/build_deps_msvc.bat
@@ -1,56 +1,54 @@
 set root=%cd%
 set common_args=-G %generator% -T %toolchain% -DCMAKE_INSTALL_PREFIX=%output%
 
-mkdir %build_dir%
-
-mkdir %build_dir%\zlib
+mkdir "%build_dir%\zlib"
 cd %build_dir%\zlib
-cmake %root%\zlib-1.2.8 %common_args%  || goto :error
+cmake "%root%\zlib-1.2.8" %common_args%  || goto :error
 cmake --build . --target INSTALL --config RelWithDebInfo  || goto :error
 
-mkdir %build_dir%\physfs
+mkdir "%build_dir%\physfs"
 cd %build_dir%\physfs
-cmake %root%\physfs-2.0.3 %common_args% -DPHYSFS_BUILD_TEST=off || goto :error
+cmake "%root%\physfs-2.0.3" %common_args% -DPHYSFS_BUILD_TEST=off || goto :error
 cmake --build . --target INSTALL --config RelWithDebInfo || goto :error
 
-mkdir %build_dir%\dumb
+mkdir "%build_dir%\dumb"
 cd %build_dir%\dumb
-cmake %root%\dumb-0.9.3 %common_args%  || goto :error
+cmake "%root%\dumb-0.9.3" %common_args%  || goto :error
 cmake --build . --target INSTALL --config RelWithDebInfo  || goto :error
 
-mkdir %build_dir%\freetype
+mkdir "%build_dir%\freetype"
 cd %build_dir%\freetype
-cmake %root%\freetype-2.5.5 %common_args%  || goto :error
+cmake "%root%\freetype-2.5.5" %common_args%  || goto :error
 cmake --build . --target INSTALL --config RelWithDebInfo  || goto :error
 
-mkdir %build_dir%\libjpeg-turbo
+mkdir "%build_dir%\libjpeg-turbo"
 cd %build_dir%\libjpeg-turbo
-cmake %root%\libjpeg-turbo-1.4.0 %common_args% -DWITH_TURBOJPEG=false  || goto :error
+cmake "%root%\libjpeg-turbo-1.4.0" %common_args% -DWITH_TURBOJPEG=false  || goto :error
 cmake --build . --target INSTALL --config RelWithDebInfo  || goto :error
 
-mkdir %build_dir%\libpng
+mkdir "%build_dir%\libpng"
 cd %build_dir%\libpng
-cmake %root%\libpng-1.6.17 %common_args%  || goto :error
+cmake "%root%\libpng-1.6.17" %common_args%  || goto :error
 cmake --build . --target INSTALL --config Release  || goto :error
 
-mkdir %build_dir%\libogg
+mkdir "%build_dir%\libogg"
 cd %build_dir%\libogg
-cmake %root%\libogg-1.3.2 %common_args%  || goto :error
+cmake "%root%\libogg-1.3.2" %common_args%  || goto :error
 cmake --build . --target INSTALL --config RelWithDebInfo  || goto :error
 
-mkdir %build_dir%\libvorbis
+mkdir "%build_dir%\libvorbis"
 cd %build_dir%\libvorbis
-cmake %root%\libvorbis-1.3.5 %common_args%  || goto :error
+cmake "%root%\libvorbis-1.3.5" %common_args%  || goto :error
 cmake --build . --target INSTALL --config RelWithDebInfo  || goto :error
 
-mkdir %build_dir%\libtheora
+mkdir "%build_dir%\libtheora"
 cd %build_dir%\libtheora
-cmake %root%\libtheora-1.1.1 %common_args%  || goto :error
+cmake "%root%\libtheora-1.1.1" %common_args%  || goto :error
 cmake --build . --target INSTALL --config RelWithDebInfo  || goto :error
 
-mkdir %build_dir%\flac
+mkdir "%build_dir%\flac"
 cd %build_dir%\flac
-cmake %root%\flac-1.3.1 %common_args%  || goto :error
+cmake "%root%\flac-1.3.1" %common_args%  || goto :error
 cmake --build . --target INSTALL --config RelWithDebInfo  || goto :error
 
 goto :success
