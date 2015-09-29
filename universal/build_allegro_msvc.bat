dd7bd2d0d3d72726a0520cd624ff519c7561c2e8
Pavel Sountsov <siege@google.com>
2015-09-28 20:29:27 -0700

    Link dependencies statically.

--- universal/build_allegro_msvc.bat
+++ universal/build_allegro_msvc.bat
@@ -1,14 +1,15 @@
 if not defined build_dir goto :nodirect
 
 set root=%cd%
-set common_args=%generator% %toolchain% -DCMAKE_PREFIX_PATH="%deps%" -DCMAKE_INSTALL_PREFIX="%output%" -DWANT_EXAMPLES=off -DWANT_TESTS=off -DWANT_DEMO=off
+set common_args=%generator% %toolchain% -DCMAKE_PREFIX_PATH="%deps%" -DCMAKE_INSTALL_PREFIX="%output%" ^
+-DWANT_EXAMPLES=off -DWANT_TESTS=off -DWANT_DEMO=off -DWANT_ACODEC_DYNAMIC_LOAD=off
 
 mkdir "%build_dir%\allegro"
 cd "%build_dir%\allegro"
 
-cmake "%root%\allegro" -DWANT_MONOLITH=off -DSHARED=on -DWANT_STATIC_RUNTIME=off -DCMAKE_BUILD_TYPE=RelWithDebInfo %common_args% || goto :error
+cmake "%root%\allegro" -DWANT_MONOLITH=off -DSHARED=on -DWANT_STATIC_RUNTIME=on -DCMAKE_BUILD_TYPE=RelWithDebInfo %common_args% || goto :error
 cmake --build . --target INSTALL --config RelWithDebInfo  || goto :error
-cmake "%root%\allegro" -DWANT_MONOLITH=on -DSHARED=on -DWANT_STATIC_RUNTIME=off -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake "%root%\allegro" -DWANT_MONOLITH=on -DSHARED=on -DWANT_STATIC_RUNTIME=on -DCMAKE_BUILD_TYPE=RelWithDebInfo
 cmake --build . --target INSTALL --config RelWithDebInfo  || goto :error
 cmake "%root%\allegro" -DWANT_MONOLITH=off -DSHARED=off -DWANT_STATIC_RUNTIME=on -DCMAKE_BUILD_TYPE=RelWithDebInfo
 cmake --build . --target INSTALL --config RelWithDebInfo  || goto :error
