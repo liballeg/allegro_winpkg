0c4f3594cbd07b5190eb8bb2b5de5f1586c6b6d3
Pavel Sountsov <siege@google.com>
2015-10-08 23:13:06 -0700

    Start working on msys scripts.

---
+++ universal/build_deps_unix.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+if [[ $# -ne 3 ]]
+then
+	echo Usage: $0 GENERATOR BUILD_DIR OUTPUT_DIR
+	echo Example: $0 '"MSYS Makefiles"' build_msys_32 allegro_deps_msys_32
+	exit 1
+fi
+
+generator=$1
+build_dir=$2
+output_dir=$3
+
+root=$(pwd)
+common_args=-DCMAKE_INSTALL_PREFIX=\"${root}/${output_dir}\"
+if [ -n "$generator" ]
+then
+	common_args="${common_args} -G \"${generator}\""
+fi
+
+error ()
+{
+	exit $?
+}
+
+set -x
+
+mkdir -p "${build_dir}/zlib"
+cd "${build_dir}/zlib"
+eval cmake "${root}/zlib-1.2.8" ${common_args} || error
+make install -j4 || error
+
+mkdir -p "${build_dir}/physfs"
+cd "${build_dir}/physfs"
+eval cmake "${root}/physfs-2.0.3" ${common_args} -DPHYSFS_BUILD_TEST=off || error
+make install -j4 || error
+
+mkdir -p "${build_dir}/dumb"
+cd "${build_dir}/dumb"
+eval cmake "${root}/dumb-0.9.3" ${common_args} || error
+make install -j4 || error
+
+mkdir -p "${build_dir}/freetype"
+cd "${build_dir}/freetype"
+eval cmake "${root}/freetype-2.5.5" ${common_args} || error
+make install -j4 || error
+
+mkdir -p "${build_dir}/libjpeg-turbo"
+cd "${build_dir}/libjpeg-turbo"
+eval cmake "${root}/libjpeg-turbo-1.4.0" ${common_args} -DWITH_TURBOJPEG=false || error
+make install -j4 || error
+
+mkdir -p "${build_dir}/libpng"
+cd ${build_dir}/libpng
+eval cmake "${root}/libpng-1.6.17" ${common_args}  || error
+make install -j4 || error
+
+mkdir -p "${build_dir}/libogg"
+cd ${build_dir}/libogg
+eval cmake "${root}/libogg-1.3.2" ${common_args}  || error
+make install -j4 || error
+
+mkdir -p "${build_dir}/libvorbis"
+cd ${build_dir}/libvorbis
+eval cmake "${root}/libvorbis-1.3.5" ${common_args}  || error
+make install -j4 || error
+
+mkdir -p "${build_dir}/libtheora"
+cd ${build_dir}/libtheora
+eval cmake "${root}/libtheora-1.1.1" ${common_args}  || error
+make install -j4 || error
+
+mkdir -p "${build_dir}/flac"
+cd ${build_dir}/flac
+eval cmake "${root}/flac-1.3.1" ${common_args}  || error
+make install -j4 || error
+
+# 
+# goto :success
+# 
+# :error
+# @echo Failed with error #%errorlevel%.
+# exit /b %errorlevel%
+# 
+# :nodirect
+# @echo Failed : use build_deps_msvc_32 or build_deps_msvc_64
+# 
+# :success
+
