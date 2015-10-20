7e6b63e91feda5e5040ecca61ed56926f5dd26d2
Peter Hull <peterhull90@gmail.com>
2015-10-20 14:42:32 +0100

    There is no 'XP' toolchain for v140 (+1 squashed commits)
    Squashed commits:
    
    [e2c0e97] There is no 'XP' toolchain for v140

--- universal/build_allegro_msvc2015_64.bat
+++ universal/build_allegro_msvc2015_64.bat
@@ -2,7 +2,7 @@ setlocal
 set deps=%cd%\allegro_deps-msvc2015-x64\allegro_deps
 set output=%cd%\allegro-msvc2015-x64\allegro
 set generator=-G "Visual Studio 14 2015 Win64"
-set toolchain=-T v140_xp
+set toolchain=-T v140
 set build_dir=%cd%\build_msvc2015_64
 
 call build_allegro_msvc.bat
