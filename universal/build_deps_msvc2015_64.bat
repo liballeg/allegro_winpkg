946bda59b2b121cb5712df36d6838049c3481b43
Peter Hull <peterhull90@gmail.com>
2015-10-20 14:26:19 +0100

    There is no 'XP' toolchain for v140

--- universal/build_deps_msvc2015_64.bat
+++ universal/build_deps_msvc2015_64.bat
@@ -1,7 +1,7 @@
 setlocal
 set output=%cd%\allegro_deps-msvc2015-x64\allegro_deps
 set generator=-G "Visual Studio 14 2015 Win64"
-set toolchain=-T v140_xp
+set toolchain=-T v140
 set build_dir=%cd%\build_msvc2015_64
 
 call build_deps_msvc.bat
