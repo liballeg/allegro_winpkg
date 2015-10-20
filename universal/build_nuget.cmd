180eebfe2e385f0521f1418eb4988a05f0ab2417
Peter Hull <peterhull90@gmail.com>
2015-10-20 14:25:49 +0100

    Update to match latest changes on upstream repo.

--- universal/build_nuget.cmd
+++ universal/build_nuget.cmd
@@ -1,6 +1,6 @@
-call build_deps_msvc_32
-call build_deps_msvc_64
-call build_allegro_msvc_32
-call build_allegro_msvc_64
+call build_deps_msvc2015_32
+call build_deps_msvc2015_64
+call build_allegro_msvc2015_32
+call build_allegro_msvc2015_64
 nuget pack Allegro.5.1.12.nuspec
 
