60240c64e42ae8923d3fb455766bdc1ce3d20bca
sub-c <79296426+sub-c@users.noreply.github.com>
2021-12-10 06:46:36 -0500

    update msvc2022 deps batch files toolchain to v143

--- universal/build_deps_msvc2022_32.bat
+++ universal/build_deps_msvc2022_32.bat
@@ -1,7 +1,7 @@
 setlocal
 set output=%cd%\allegro_deps-msvc2022-x86\allegro_deps
 set generator=-G "Visual Studio 17 2022" -A Win32
-set toolchain=-T v141_xp
+set toolchain=-T v143
 set build_dir=%cd%\build_msvc2022_32
 
 call build_deps_msvc.bat
